037ef15c0bbbd36c8d189643755808829b309d1a
abousiyd <abdou.bousiyd@gmail.com>
2022-06-01 15:59:32 +0200

    relating app to api

--- staff/abdou-bousiyd/notesPromise/src/helpers/generateToken.js
+++
@@ -1,9 +0,0 @@
-const { sign } = require('jsonwebtoken')
-
-function generateToken(userId) {
-    const token = sign({ sub: userId }, 'a pepito le gusta el nudismo')
-
-    return token
-}
-
-module.exports = generateToken
